f198eab98778fd919586cf01ac3076efbf45ad3c
Dresvyankin <dresvyankin1995@gmail.com>
2020-10-01 20:41:32 +0300

    switch

--- hw6arr.obj/objTask.js
+++ hw6arr.obj/objTask.js
@@ -7,13 +7,23 @@ let person = {
   height: undefined,
 };
 for (let key in person) {
-  if (typeof person[key] === "string") {
-    person[key] = person[key].toLowerCase();
-  } else if (typeof person[key] === "number") {
-    delete person[key];
-  } else if (typeof person[key] === "undefined") {
-    person[key] = 999;
+  switch (typeof person[key]) {
+    case "string":
+      person[key] = person[key].toLowerCase();
+      break;
+    case "number":
+      delete person[key];
+      break;
+    case "undefined":
+      person[key] = 999;
   }
 }
 
+//if (typeof person[key] === "string") {
+//   person[key] = person[key].toLowerCase();
+// } else if (typeof person[key] === "number") {
+//   delete person[key];
+// } else if (typeof person[key] === "undefined") {
+//   person[key] = 999;
+// }
 console.log(person);
